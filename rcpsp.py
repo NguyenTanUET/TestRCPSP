06cf6a1dc481c14d53d1a0dcea11bf03f70c0ef0
SeeringC <nguyentanabc91@gmail.com>
2025-06-03 11:08:25 +0700

    fix

--- rcpsp.py
+++ rcpsp.py
@@ -105,6 +105,21 @@ res = mdl.solve(FailLimit=1000000, TimeLimit=10)
 print('Solution: ')
 res.print_solution()
 
+from google.cloud import storage
+import os
+
+# Tên bucket mà bạn đã tạo
+bucket_name = "rcpsp-results-bucket"
+client = storage.Client()
+bucket = client.bucket(bucket_name)
+
+local_path = "result/test.csv"
+blob_name = f"results/{os.path.basename(local_path)}"  # ví dụ "results/j30_no_bound_1200s.csv"
+
+blob = bucket.blob(blob_name)
+blob.upload_from_filename(local_path)
+print(f"Uploaded {local_path} to gs://{bucket_name}/{blob_name}")
+
 
 # import docplex.cp.utils_visu as visu
 #
